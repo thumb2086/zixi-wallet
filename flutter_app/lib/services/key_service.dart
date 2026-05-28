c8ba853c93c7b466cfb9375a3d4c0342779c761b
thumb2086 <cpxru83@gmail.com>
2026-05-28 22:54:26 +0800

    fix: manual PBKDF2 implementation to resolve pointycastle import

--- flutter_app/lib/services/key_service.dart
+++ flutter_app/lib/services/key_service.dart
@@ -4,8 +4,6 @@ import 'dart:typed_data';
 
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:pointycastle/export.dart' hide Padding, State;
-import 'package:pointycastle/src/key_derivation/derivation_parameters.dart';
-import 'package:pointycastle/src/key_derivation/salt_and_iterations_parameter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:web3dart/crypto.dart' as web3crypto;
 import 'package:web3dart/web3dart.dart';
@@ -270,9 +268,47 @@ class KeyService {
 
   Uint8List _deriveKey(String pin, Uint8List salt) {
     final password = Uint8List.fromList(utf8.encode(pin));
-    final derivator = PBKDF2KeyDerivator(HMac(SHA256Digest(), 64));
-    derivator.init(DerivationParameters(SaltAndIterationsParameter(salt, _pbkdf2Iterations)));
-    return derivator.process(password);
+    final hmac = HMac(SHA256Digest(), 64);
+    final block1 = _pbkdf2Block(hmac, password, salt, _pbkdf2Iterations, 1);
+    return Uint8List.fromList(block1.sublist(0, 32));
+  }
+
+  Uint8List _pbkdf2Block(HMac hmac, Uint8List password, Uint8List salt, int iterations, int blockIndex) {
+    final block = _int32Bytes(blockIndex);
+
+    Uint8List u = Uint8List(salt.length + block.length);
+    u.setRange(0, salt.length, salt);
+    u.setRange(salt.length, u.length, block);
+
+    hmac.init(KeyParameter(password));
+    u = _hmacDigest(hmac, u);
+    Uint8List result = Uint8List.fromList(u);
+
+    for (var i = 1; i < iterations; i++) {
+      hmac.init(KeyParameter(password));
+      u = _hmacDigest(hmac, u);
+      for (var j = 0; j < result.length; j++) {
+        result[j] ^= u[j];
+      }
+    }
+
+    return result;
+  }
+
+  Uint8List _hmacDigest(HMac hmac, Uint8List data) {
+    hmac.update(data, 0, data.length);
+    final out = Uint8List(hmac.macSize);
+    hmac.doFinal(out, 0);
+    return out;
+  }
+
+  Uint8List _int32Bytes(int value) {
+    return Uint8List.fromList([
+      (value >> 24) & 0xFF,
+      (value >> 16) & 0xFF,
+      (value >> 8) & 0xFF,
+      value & 0xFF,
+    ]);
   }
 
   Uint8List _aesGcmEncrypt(Uint8List plaintext, Uint8List key, Uint8List nonce) {
